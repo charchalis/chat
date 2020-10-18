5534aa6a6df2983100e03c41592b2b434e7163af
charchalis <charchalis97@gmail.com>
2020-10-18 19:19:48 +0100

    adunow

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Text, TextInput, View, Button } from 'react-native';
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import { GiftedChat } from 'react-native-gifted-chat';
 import io from "socket.io-client";
 
 const socket = io("http://192.168.1.129:3000");
@@ -9,28 +10,28 @@ var messages = []
 
 socket.on('gimme messages', msgs => {
   messages = msgs;
+  console.log("thank you");
 });
-
-socket.emit('gimme messages');
 
 socket.on("user message", userMessage => {
   console.log("Got message from socket: ");
-  console.log(userMessage);
+  console.log("username: ", userMessage.username, "message: ", userMessage.message);
   messages.push(userMessage);
 });
 
 function jsxifyMessage(userMessage){
-  console.log("made it here");
   return (
     <Text key={userMessage.message}> username: {userMessage.username}, message: {userMessage.message} </Text>
   );
 }
 
 const MessagesJsx = () => {
+
   return messages.map((message) => {
-    return jsxifyMessage(message)
-  })
+    return jsxifyMessage(message);
+  });
 }
+
 
 const Login = ({navigation}) => {
 
@@ -43,8 +44,10 @@ const Login = ({navigation}) => {
       <TextInput style={styles.textInput} secureTextEntry={true} onChangeText={setPassword}/>
       <Button
         title="Login"
-        onPress={() => {
+        onPress={async () => {
           socket.emit('user data', {username: username, password: password});
+
+          await new Promise(r => setTimeout(r, 2000));
 
           navigation.navigate('Home', {username: username});
         }}
@@ -60,23 +63,34 @@ function HomeScreen({ navigation} ) {
 
   const username =  navigation.state.params.username;
 
+  var msgsJsx = MessagesJsx();
+
+  console.log("messages.length: ", messages.length);
+
+  useEffect(() => {
+    console.log("updatou");
+    msgsJsx = MessagesJsx();
+  });
+
   return (
     <View style = {styles.container}>
-      {MessagesJsx()}
-      <TextInput style={styles.textInput} onChangeText={setMessage} type="reset"/>
-      <Button
-        title="send"
-        onPress={() => {
+      <View>
+        {msgsJsx}
+      </View>
+      <View>
+        <TextInput style={styles.textInput} onChangeText={setMessage} type="reset"/>
+        <Button
+          title="send"
+          onPress={async () => {
 
-          const userMessage = {username: username, message: message}
+            const userMessage = {username: username, message: message}
 
-          socket.emit('user message', {username: username, message: message});
-
-          messages.push(userMessage);
-
-          setMessage('');
-        }}
-      />
+            socket.emit('user message', userMessage); //sended message to server. it will now receive message and push it to message array
+            
+            await new Promise(r => setTimeout(r, 2000));
+          }}
+        />
+      </View>
     </View>
   )
 
--- server/index.js
+++ server/index.js
@@ -4,19 +4,22 @@ const server = require("http").createServer(app);
 const io = require("socket.io").listen(server);
 const port = 3000;
 
-var messages = [];
+var messages = [{username: "pre mensagens", message: "a pintar"}];
 
 io.on("connection", socket => {
     console.log("a user connected :D");
     socket.on("user data", userData => {
       console.log(userData);
+      console.log('sending messages');
+      socket.emit('gimme messages', messages);
+      console.log("there u go");
     });
     socket.on("user message", userMessage => {
         console.log(userMessage);
         messages.push(userMessage);
         console.log(messages.length);
-        socket.broadcast.emit("user message", userMessage);
-    })
+        io.emit("user message", userMessage);
+    });
   });
 
   server.listen(port, () => console.log("server running on port:" + port));
